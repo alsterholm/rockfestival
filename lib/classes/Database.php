e981a7a54f32c5633483f01e58ad8d70b6aed4a9
Andreas Indahl <andreas@rocketship.se>
2015-04-27 10:21:50 +0200

    Bugfix

--- lib/classes/Database.php
+++ lib/classes/Database.php
@@ -7,7 +7,7 @@
 
 		private function __construct() {
 			$this->pdo = new PDO(
-				'mysql.host=localhost;dbname=rockfestival',
+				'mysql:host=localhost;dbname=rockfestival',
 				'root',
 				'',
 				array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
@@ -23,19 +23,19 @@
 
 		public function query($sql, $params = array()) {
 			$this-> _error = false;
-			if($this->_query = $this->_pdo->prepare($sql)) {
+			if($this->query = $this->pdo->prepare($sql)) {
 				$x = 1;
 				if(count($params)){
 					foreach($params as $param){
-						$this->_query->bindValue($x, $param);
+						$this->query->bindValue($x, $param);
 						$x++;
 					}
 				}
-				if($this->_query->execute()){
-					$this->_result = $this->_query->fetchAll(PDO::FETCH_OBJ);
-					$this->_count = $this->_query->rowCount();
+				if($this->query->execute()){
+					$this->result = $this->query->fetchAll(PDO::FETCH_OBJ);
+					$this->count = $this->query->rowCount();
 				}else{
-					$this->_error = true;
+					$this->error = true;
 				}
 			}
 			return $this;

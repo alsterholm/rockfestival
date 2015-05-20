00c14463eaa85e419439afea7effc7adf47b0b06
Andreas Indahl <andreas@rocketship.se>
2015-05-20 14:40:00 +0200

    Databas

--- rockfestival.sql
+++ rockfestival.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 27 apr 2015 kl 12:31
--- Serverversion: 5.6.21
--- PHP-version: 5.6.3
+-- Tid vid skapande: 20 maj 2015 kl 14:39
+-- Serverversion: 5.6.24
+-- PHP-version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,17 +27,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `anstalld` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `namn` varchar(50) NOT NULL,
   `personnummer` varchar(13) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumpning av Data i tabell `anstalld`
 --
 
 INSERT INTO `anstalld` (`id`, `namn`, `personnummer`) VALUES
-(1, 'Sven Eriksson', '19830426-1234');
+(1, 'Sven Eriksson', '19830426-1234'),
+(2, 'Jimmy Lindström', '19790730-1234'),
+(3, 'Andreas Indal', '19931222-1234'),
+(4, 'Lisa Svensson', '19840227-2345');
 
 -- --------------------------------------------------------
 
@@ -46,12 +49,23 @@ INSERT INTO `anstalld` (`id`, `namn`, `personnummer`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `band` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `namn` varchar(50) NOT NULL,
   `genre` varchar(50) NOT NULL,
   `land` varchar(50) NOT NULL,
   `kontaktperson` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `band`
+--
+
+INSERT INTO `band` (`id`, `namn`, `genre`, `land`, `kontaktperson`) VALUES
+(1, 'Blomstermåla Rockers', 'Blues', 'Sverige', 1),
+(2, 'Metallica', 'Thrash metal', 'USA', 3),
+(3, 'Foo Fighters', 'Rock', 'USA', 2),
+(4, 'Iron Maiden', 'Heavy metal', 'UK', 2),
+(5, 'Young Guns', 'Alternativ', 'UK', 3);
 
 -- --------------------------------------------------------
 
@@ -60,11 +74,42 @@ CREATE TABLE IF NOT EXISTS `band` (
 --
 
 CREATE TABLE IF NOT EXISTS `bandmedlem` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `namn` varchar(50) NOT NULL,
   `fdatum` date NOT NULL,
   `band_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `bandmedlem`
+--
+
+INSERT INTO `bandmedlem` (`id`, `namn`, `fdatum`, `band_id`) VALUES
+(1, 'Lars Ulrich', '1900-11-20', 2),
+(2, 'James Hetfield', '1900-11-20', 2),
+(3, 'Robert Trujillo', '1900-11-20', 2),
+(4, 'Kirk Hammett', '1900-11-20', 2),
+(5, 'Dave Grohl', '1900-11-20', 3),
+(6, 'Taylor Hawkins', '1900-11-20', 3),
+(7, 'Nate Mendel', '1900-11-20', 3),
+(8, 'Chris Shiflett', '1900-11-20', 3),
+(9, 'Patt Smear', '1900-11-20', 3),
+(10, 'Björn Johansson', '1900-11-20', 1),
+(11, 'Glenn Glenn', '1900-11-20', 1),
+(12, 'Göran Andersson', '1900-11-20', 1),
+(13, 'Berit Ljung', '1900-11-20', 1),
+(14, 'Jonas Ljung', '1900-11-20', 1),
+(15, 'Steve Harris', '1900-11-20', 4),
+(16, 'Dave Murray', '1900-11-20', 4),
+(17, 'Bruce Dickinson', '1900-11-20', 4),
+(18, 'Adrian Smith', '1900-11-20', 4),
+(19, 'Nicko McBrain', '1900-11-20', 4),
+(20, 'Janick Gers', '1900-11-20', 4),
+(21, 'Gustav Wood', '1900-01-01', 5),
+(22, 'Fraser Taylor', '1900-01-01', 5),
+(23, 'John Taylor', '1900-01-01', 5),
+(24, 'Simon Mitchell', '1900-01-01', 5),
+(25, 'Ben Joliffe', '1900-01-01', 5);
 
 -- --------------------------------------------------------
 
@@ -81,10 +126,12 @@ CREATE TABLE IF NOT EXISTS `genre` (
 --
 
 INSERT INTO `genre` (`namn`) VALUES
+('Alternativ'),
 ('Blues'),
 ('Heavy metal'),
 ('Indierock'),
-('Rock');
+('Rock'),
+('Thrash metal');
 
 -- --------------------------------------------------------
 
@@ -93,12 +140,23 @@ INSERT INTO `genre` (`namn`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `sakerhetsansvar` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `anstalld_id` int(11) NOT NULL,
   `scen_id` int(11) NOT NULL,
   `starttid` datetime NOT NULL,
   `sluttid` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `sakerhetsansvar`
+--
+
+INSERT INTO `sakerhetsansvar` (`id`, `anstalld_id`, `scen_id`, `starttid`, `sluttid`) VALUES
+(1, 1, 2, '2015-06-18 12:00:00', '2015-06-18 16:00:00'),
+(2, 2, 2, '2015-06-18 08:00:00', '2015-06-18 12:00:00'),
+(3, 3, 1, '2015-06-18 12:00:00', '2015-06-18 16:00:00'),
+(4, 1, 1, '2015-06-18 16:00:00', '2015-06-18 20:00:00'),
+(5, 4, 1, '2015-06-18 08:00:00', '2015-06-18 12:00:00');
 
 -- --------------------------------------------------------
 
@@ -107,7 +165,7 @@ CREATE TABLE IF NOT EXISTS `sakerhetsansvar` (
 --
 
 CREATE TABLE IF NOT EXISTS `scen` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `namn` varchar(50) NOT NULL,
   `kapacitet` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
@@ -128,12 +186,22 @@ INSERT INTO `scen` (`id`, `namn`, `kapacitet`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `spelschema` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `band_id` int(11) NOT NULL,
   `scen_id` int(11) NOT NULL,
   `starttid` datetime NOT NULL,
   `sluttid` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `spelschema`
+--
+
+INSERT INTO `spelschema` (`id`, `band_id`, `scen_id`, `starttid`, `sluttid`) VALUES
+(3, 1, 1, '2015-06-18 12:00:00', '2015-06-18 15:00:00'),
+(5, 3, 1, '2015-06-18 09:00:00', '2015-06-18 12:00:00'),
+(6, 2, 2, '2015-06-18 18:00:00', '2015-06-18 21:00:00'),
+(7, 4, 1, '2015-06-19 18:00:00', '2015-06-19 21:00:00');
 
 --
 -- Index för dumpade tabeller
@@ -143,43 +211,43 @@ CREATE TABLE IF NOT EXISTS `spelschema` (
 -- Index för tabell `anstalld`
 --
 ALTER TABLE `anstalld`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `personnummer` (`personnummer`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `personnummer` (`personnummer`);
 
 --
 -- Index för tabell `band`
 --
 ALTER TABLE `band`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `namn` (`namn`), ADD KEY `genre` (`genre`), ADD KEY `kontaktperson` (`kontaktperson`), ADD KEY `kontaktperson_2` (`kontaktperson`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `namn` (`namn`), ADD KEY `genre` (`genre`), ADD KEY `kontaktperson` (`kontaktperson`), ADD KEY `kontaktperson_2` (`kontaktperson`);
 
 --
 -- Index för tabell `bandmedlem`
 --
 ALTER TABLE `bandmedlem`
- ADD PRIMARY KEY (`id`), ADD KEY `band_id` (`band_id`);
+  ADD PRIMARY KEY (`id`), ADD KEY `band_id` (`band_id`);
 
 --
 -- Index för tabell `genre`
 --
 ALTER TABLE `genre`
- ADD PRIMARY KEY (`namn`);
+  ADD PRIMARY KEY (`namn`);
 
 --
 -- Index för tabell `sakerhetsansvar`
 --
 ALTER TABLE `sakerhetsansvar`
- ADD PRIMARY KEY (`id`), ADD KEY `anstalld_id` (`anstalld_id`), ADD KEY `scen_id` (`scen_id`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `sakerhetsansvar_person_tid` (`anstalld_id`,`starttid`), ADD UNIQUE KEY `sakerhetsansvar_scen_tid` (`scen_id`,`starttid`), ADD KEY `anstalld_id` (`anstalld_id`), ADD KEY `scen_id` (`scen_id`);
 
 --
 -- Index för tabell `scen`
 --
 ALTER TABLE `scen`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index för tabell `spelschema`
 --
 ALTER TABLE `spelschema`
- ADD PRIMARY KEY (`id`), ADD KEY `band_id` (`band_id`), ADD KEY `scen_id` (`scen_id`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `spelschema_scen_tid` (`scen_id`,`starttid`), ADD KEY `band_id` (`band_id`), ADD KEY `scen_id` (`scen_id`);
 
 --
 -- AUTO_INCREMENT för dumpade tabeller
@@ -189,32 +257,32 @@ ALTER TABLE `spelschema`
 -- AUTO_INCREMENT för tabell `anstalld`
 --
 ALTER TABLE `anstalld`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT för tabell `band`
 --
 ALTER TABLE `band`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT för tabell `bandmedlem`
 --
 ALTER TABLE `bandmedlem`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT för tabell `sakerhetsansvar`
 --
 ALTER TABLE `sakerhetsansvar`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT för tabell `scen`
 --
 ALTER TABLE `scen`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT för tabell `spelschema`
 --
 ALTER TABLE `spelschema`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- Restriktioner för dumpade tabeller
 --
